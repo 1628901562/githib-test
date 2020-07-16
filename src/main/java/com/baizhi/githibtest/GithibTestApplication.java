30e43649417ad029a121108310b25524e76194de
yangs <1628901562@qq.com>
2020-07-16 21:10:57 +0800

    这是第一个idea项目 v1.0

---
+++ src/main/java/com/baizhi/githibtest/GithibTestApplication.java
@@ -0,0 +1,13 @@
+package com.baizhi.githibtest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GithibTestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GithibTestApplication.class, args);
+    }
+
+}
